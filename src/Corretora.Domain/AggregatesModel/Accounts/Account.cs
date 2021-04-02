763eb6d013222eb574a33631fed91988ea39a447
Luis Otávio <luis.avelar@squadra.com.br>
2021-04-02 18:24:21 -0300

    Criação de metodos para depósito e saque na conta corrente

--- src/Corretora.Domain/AggregatesModel/Accounts/Account.cs
+++ src/Corretora.Domain/AggregatesModel/Accounts/Account.cs
@@ -1,4 +1,6 @@
-﻿using Corretora.Domain.SeedWork;
+﻿using Corretora.Domain.AggregatesModel.Accounts.Rules;
+using Corretora.Domain.SeedWork;
+using System;
 
 namespace Corretora.Domain.AggregatesModel.Accounts
 {
@@ -7,5 +9,29 @@ namespace Corretora.Domain.AggregatesModel.Accounts
         public AccountNumber Number { get; private set; }
 
         public decimal Balance { get; private set; }
+
+        public Guid UserId { get; private set; }
+
+        protected Account() { }
+
+        private Account(AccountNumber number)
+        {
+            Number = number;
+            Balance = 0;
+        }
+
+        public void Deposit(decimal value)
+        {
+            CheckRule(new DepositMustBeGreaterThanZeroRule());
+
+            Balance += value;
+        }
+
+        public void Withdraw(decimal value)
+        {
+            CheckRule(new DepositMustBeGreaterThanZeroRule());
+
+            Balance -= value;
+        }       
     }
 }
---
+++ src/Corretora.Domain/AggregatesModel/Accounts/Rules/DepositMustBeGreaterThanZeroRule.cs
@@ -0,0 +1,14 @@
+﻿using Corretora.Domain.SeedWork;
+
+namespace Corretora.Domain.AggregatesModel.Accounts.Rules
+{
+    public class DepositMustBeGreaterThanZeroRule : IBusinessRule
+    {
+        public string Message => throw new System.NotImplementedException();
+
+        public bool IsBroken()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
