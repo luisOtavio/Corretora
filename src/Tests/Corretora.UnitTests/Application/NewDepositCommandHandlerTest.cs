74443cd51516e918fa976c3aa3a0979fafdc5164
Luis Otávio <luis.avelar@squadra.com.br>
2021-04-10 15:54:13 -0300

    Criação dos testes para o hander de realização de novos depósitos

---
+++ src/Tests/Corretora.UnitTests/Application/NewDepositCommandHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿using Corretora.Domain.AggregatesModel.Accounts;
+using Corretora.Domain.AggregatesModel.Users;
+using MediatR;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Corretora.UnitTests.Application
+{
+    public class NewDepositCommandHandlerTest
+    {
+        private readonly Mock<IAccountRepository> _orderRepositoryMock;
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IMediator> _mediator;
+
+        public NewDepositCommandHandlerTest()
+        {
+
+            _orderRepositoryMock = new Mock<IAccountRepository>();
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _mediator = new Mock<IMediator>();
+        }
+
+        [Fact]
+        public async Task NewDepositCommandHandler_AccountNotFound_MustThrowException()
+        {
+            
+        }
+    }
+}
