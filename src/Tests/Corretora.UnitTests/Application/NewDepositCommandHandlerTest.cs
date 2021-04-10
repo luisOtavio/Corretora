d3e32fe0647ff2eb4b3fec8cc5ca153f0f02d8e1
Luis Otávio <luis.avelar@squadra.com.br>
2021-04-10 16:51:17 -0300

    Implementação do teste para verificar exceção quando a conta não é encontrada

--- src/Tests/Corretora.UnitTests/Application/NewDepositCommandHandlerTest.cs
+++ src/Tests/Corretora.UnitTests/Application/NewDepositCommandHandlerTest.cs
@@ -1,7 +1,8 @@
-﻿using Corretora.Domain.AggregatesModel.Accounts;
+﻿using Corretora.Application.Accounts.Deposit;
+using Corretora.Domain.AggregatesModel.Accounts;
 using Corretora.Domain.AggregatesModel.Users;
-using MediatR;
 using Moq;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -9,22 +10,38 @@ namespace Corretora.UnitTests.Application
 {
     public class NewDepositCommandHandlerTest
     {
-        private readonly Mock<IAccountRepository> _orderRepositoryMock;
+        private readonly Mock<IAccountRepository> _accountRepositoryMock;
         private readonly Mock<IUserRepository> _userRepositoryMock;
-        private readonly Mock<IMediator> _mediator;
 
         public NewDepositCommandHandlerTest()
         {
-
-            _orderRepositoryMock = new Mock<IAccountRepository>();
+            _accountRepositoryMock = new Mock<IAccountRepository>();
             _userRepositoryMock = new Mock<IUserRepository>();
-            _mediator = new Mock<IMediator>();
         }
 
         [Fact]
         public async Task NewDepositCommandHandler_AccountNotFound_MustThrowException()
         {
-            
+            // Arrange
+            _accountRepositoryMock
+                .Setup(accountRepository => accountRepository.FindByNumberAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult<Account>(null));
+
+            var command = new NewDepositCommand
+            {
+                Target = new TargetDto
+                {
+                    Account = ""
+                }
+            };
+
+            var handler = new NewDepositCommandHandler(_accountRepositoryMock.Object, _userRepositoryMock.Object);
+
+            var cltToken = new System.Threading.CancellationToken();
+
+            // Act && Assert
+            await Assert.ThrowsAsync<ApplicationException>(async () => await handler.Handle(command, cltToken));
+
         }
     }
 }
