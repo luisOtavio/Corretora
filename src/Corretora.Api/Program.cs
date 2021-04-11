9615ae7f3fda29445980597b32f6e8c5b63666f8
Luis Otávio <luis.avelar@squadra.com.br>
2021-04-11 16:29:06 -0300

    Configuração do serilog

--- src/Corretora.Api/Program.cs
+++ src/Corretora.Api/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Corretora.Api
 {
@@ -13,14 +13,44 @@ namespace Corretora.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var configuration = GetConfiguration(scope.ServiceProvider.GetService<IWebHostEnvironment>());
+                Log.Logger = CreateSerilogLogger(configuration);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Seq(configuration["Serilog:SeqServerUrl"])
+                .WriteTo.Console(new RenderedCompactJsonFormatter())
+                .WriteTo.Debug(outputTemplate: DateTime.Now.ToString())
+                .CreateLogger();
+        }
+
+        private static IConfiguration GetConfiguration(IWebHostEnvironment webHostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(webHostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{webHostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
     }
+
 }
